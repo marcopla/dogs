380e0025b0c28f5e2f1f43c676534b0e8bdeb62b
Marco Pla <marco.pla@gmail.com>
2021-02-18 19:17:20 -0300

    NPM reinstall

---
+++ react-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <a href="https://www.origamid.com" title="Site Origamid">
+        Origamid
+      </a>
+    </React.Fragment>
+  );
+};
+
+export default App;
