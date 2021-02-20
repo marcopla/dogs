eb245e92d15e70d39369caea149bd802448da372
Marco Pla <marco.pla@gmail.com>
2021-02-19 23:50:09 -0300

    array exercice

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Exercice from './Exercice';
+import JSXArrays from './jsxarrays.js';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
     <Exercice />
+    <JSXArrays />
   </React.StrictMode>,
   document.getElementById('root'),
 );
