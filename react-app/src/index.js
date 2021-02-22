663f69eff3613dc0bbed32a4f20060d35552d30d
Marco Pla <marco.pla@gmail.com>
2021-02-21 23:46:31 -0300

    React Eventos

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Exercice from './Exercice';
-import JSXArrays from './jsxarrays.js';
+import JSXArrays from './jsxarrays';
+import Eventos from './Eventos';
 
 ReactDOM.render(
   <React.StrictMode>
-    <JSXArrays />
+    <Eventos />
   </React.StrictMode>,
   document.getElementById('root'),
 );
