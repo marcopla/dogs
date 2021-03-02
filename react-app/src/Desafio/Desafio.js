1798e1f7d3be4cbba39dd2078b1ac73c844d0ac2
Marco Pla <marco.pla@gmail.com>
2021-03-01 23:45:11 -0300

    desafio started

---
+++ react-app/src/Desafio/Desafio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Desafio = () => {
+  const produtos = [
+    { nome: 'Notebook', propriedades: ['16gb ram', '512gb'] },
+    { nome: 'Smartphone', propriedades: ['2gb ram', '128gb'] },
+  ];
+
+  return <section></section>;
+};
+
+export default Desafio;
