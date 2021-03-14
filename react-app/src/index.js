86fa32a55d0e818161334d49bea8db4f958e9288
Marco Pla <marco.pla@gmail.com>
2021-03-13 22:13:17 -0300

    useState3

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './useState/App';
+import Exercice from './useState/Exercice/Exercice';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Exercice />
   </React.StrictMode>,
   document.getElementById('root'),
 );
