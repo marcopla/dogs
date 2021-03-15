de140fa944110889fc96aff409d0ce9c2da0d06c
Marco Pla <marco.pla@gmail.com>
2021-03-14 21:41:06 -0300

    fetch product button

--- react-app/src/useState/Exercice/Exercice.js
+++ react-app/src/useState/Exercice/Exercice.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Button from './Button';
+import Produto from './Produto';
 
 const Exercice = () => {
+  const [dados, setDados] = React.useState(null);
+
+  async function handleClick(event) {
+    const response = await fetch(
+      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
+    );
+    console.log(response);
+    const json = response.json();
+    console.log(json);
+  }
   return (
     <div>
-      <Button name="notebook" />
+      <button onClick={handleClick}>notebook</button>
+      <Button name="notebook" onClick={handleClick} />
       <Button name="smartphone" />
       <Button name="tablet" />
+      <Produto />
     </div>
   );
 };
