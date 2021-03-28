f1ca42adb8c14b7e6935d3d70e502db78a028d8a
Marco Pla <marco.pla@gmail.com>
2021-03-27 21:24:16 -0300

    useEffect dependency

--- react-app/src/useEffect/App.js
+++ react-app/src/useEffect/App.js
@@ -4,14 +4,20 @@ import Produto from './Produto';
 const App = () => {
   const [ativo, setAtivo] = React.useState(false);
   const [contar, setContar] = React.useState(0);
+  console.log('executou fora');
   React.useEffect(() => {
+    document.title = 'Total ' + contar;
     console.log('Ocorre ao renderizar e ao atualizar');
-  }, []);
+  }, [contar]);
   return (
     <div>
-      <button onClick={() => setContar(contar + 1)}>{contar}</button>
+      <button style={{ margin: '.5rem' }} onClick={() => setContar(contar + 1)}>
+        {contar}
+      </button>
       {ativo && <Produto />}
-      <button onClick={() => setAtivo(!ativo)}>Ativar</button>
+      <button style={{ margin: '.5rem' }} onClick={() => setAtivo(!ativo)}>
+        Ativar
+      </button>
     </div>
   );
 };
