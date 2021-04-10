ea1c4e4328fe0745123e991bc70d7fe523407cba
Marco Pla <marco.pla@gmail.com>
2021-04-09 23:43:23 -0300

    .now() applied

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 const App = () => {
   const [contar, setContar] = React.useState(0);
+  const t1 = performance.now();
+
+  console.log(performance.now() - t1);
+
   const valor = React.useMemo(() => {
     const localStorageItem = window.localStorage.getItem('produto');
     console.log('Ocorreu nemo');
@@ -9,7 +13,12 @@ const App = () => {
 
   return (
     <div>
-      <button onClick={() => setContar(contar + 1)}>{contar}</button>
+      <button
+        style={{ marginRight: '1rem' }}
+        onClick={() => setContar(contar + 1)}
+      >
+        {contar}
+      </button>
     </div>
   );
 };
