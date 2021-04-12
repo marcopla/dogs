5eb64228d84c087bb39d74df365ff2dfee407fcf
Marco Pla <marco.pla@gmail.com>
2021-04-11 23:18:16 -0300

    useCallback added to index.js

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './useState/App';
 import Exercice from './useState/Exercice/Exercice';
+import useCallback from './useCallback';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <useCallback />
   </React.StrictMode>,
   document.getElementById('root'),
 );
