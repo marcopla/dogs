4573af3fb62f133f2f6204b3ae52ac4d82750c24
Marco Pla <marco.pla@gmail.com>
2021-04-12 23:05:39 -0300

    Set() added

--- react-app/src/useCallback.js
+++ react-app/src/useCallback.js
@@ -1,6 +1,21 @@
 import React from 'react';
+
 const useCallback = () => {
   const [contar, setContar] = React.useState(0);
+
+  const func1 = () => {
+    console.log('Teste');
+  };
+
+  const func2 = React.useCallback(() => {
+    console.log('Teste');
+  }, []);
+
+  const set1 = new Set();
+  const set2 = new Set();
+
+  set1.add(func1);
+  set2.add(func2);
   const handleClick = React.useCallback(() => {
     setContar((contar) => contar + 1);
   }, []);
