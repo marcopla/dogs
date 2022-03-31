04cb04274d4aa0316359e0d5151e75a30acdb2ea
Marco Pla <marco.pla@gmail.com>
2022-03-30 21:22:44 -0300

    produtos added

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,5 +4,6 @@ import ReactDOM from 'react-dom';
 import App from './useState/App';
 import Exercice from './useState/Exercice/Exercice';
 import useCallback from './useCallback';
+import Produto from './Produto';
 
 ReactDOM.render(<useCallback />, document.getElementById('root'));
