a3d9f936c1093c5df3158fce9fe0bb7832066581
Marco Pla <marco.pla@gmail.com>
2022-04-27 23:37:31 -0300

    produto.nome added

--- react-app/src/ExercicioUseState.js
+++ react-app/src/ExercicioUseState.js
@@ -1,33 +1,36 @@
 import React from 'react';
+import Produto from './Desafio/Produto';
 
 const ExercicioUseState = () => {
   const [tablet, setTablet] = React.useState(false);
   const [smartphone, setSmartphone] = React.useState(false);
   const [notebook, setNotebook] = React.useState(false);
 
+  let produto = { nome: 'Não definido' };
+
   function getTablet() {
     fetch('https://ranekapi.origamid.dev/json/api/produto/tablet')
       .then((response) => response.json())
-      .then((json) => console.log(json.nome));
+      .then((json) => (produto.nome = json.nome));
   }
 
   function getSmartPhone() {
     fetch('https://ranekapi.origamid.dev/json/api/produto/smartphone')
       .then((response) => response.json())
-      .then((json) => console.log(json.nome));
+      .then((json) => (produto.nome = json.nome));
   }
 
   function getNotebook() {
     fetch('https://ranekapi.origamid.dev/json/api/produto/notebook')
       .then((response) => response.json())
-      .then((json) => console.log(json.nome));
+      .then((json) => (produto.nome = json.nome));
   }
   return (
     <div>
+      {produto.nome}
       <button onClick={getTablet}>Tablet</button>
       <button onClick={getSmartPhone}>SmartPhone</button>
       <button onClick={getNotebook}>getNotebook</button>
-      <Produto />
     </div>
   );
 };
