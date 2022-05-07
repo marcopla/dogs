b02868d49ac8de228dddbd7ecc2896db1772f546
Marco Pla <marco.pla@gmail.com>
2022-05-06 23:36:32 -0300

    testing handleClick function

--- react-app/src/Produto.js
+++ react-app/src/Produto.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Produto = ({ nome }) => {
   const [dados, setDados] = React.useState(null);
 
-  function handleClick() {}
+  function handleClick(event) {
+    console.log(event.target.innerText);
+  }
 
   return (
     <div>
