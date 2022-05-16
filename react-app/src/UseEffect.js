28f28b98ee1bb9a5b5059f236be3e1617f65c8d3
Marco Pla <marco.pla@gmail.com>
2022-05-15 23:54:53 -0300

    useEffect dependence

--- react-app/src/UseEffect.js
+++ react-app/src/UseEffect.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const UseEffect = () => {
   const [contar, setContar] = React.useState(0);
+  React.useEffect(() => console.log('Ocorre ao renderizar'), []);
   return <button onClick={() => setContar(contar + 1)}>{contar}</button>;
 };
 
