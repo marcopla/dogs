ac1de5f110ef32b8a3033252a0d2cd439a724bb3
Marco Pla <marco.pla@gmail.com>
2022-05-18 23:50:22 -0300

    url added to fetch

--- react-app/src/UseEffect.js
+++ react-app/src/UseEffect.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const UseEffect = () => {
   const [contar, setContar] = React.useState(0);
+  const [dados, setDados] = React.useState(null);
   React.useEffect(() => console.log('Ocorre ao renderizar'), []);
   React.useEffect(() => (document.title = 'Total' + contar), [contar]);
   React.useEffect(() => {
-    fetch('').then((response) =>
-      response.json().then((json) => setDados(json)),
-    );
+    fetch('https://ranekapi.origamid.dev/json/api/produto/notebook')
+      .then((response) => response.json())
+      .then((json) => setDados(json));
   });
   return <button onClick={() => setContar(contar + 1)}>{contar}</button>;
 };
