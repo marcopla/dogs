3c622b5d55004dfd3513149a8623486bae133f57
Marco Pla <marco.pla@gmail.com>
2022-05-27 23:48:16 -0300

    useEffect map

--- react-app/src/useRef.js
+++ react-app/src/useRef.js
@@ -2,11 +2,20 @@ import React from 'react';
 
 const useRef = () => {
   const video = React.useRef();
+  const [comentarios, setComentarios] = React.useState(['']);
 
   React.useEffect(() => {
     console.log(video.current);
   }, []);
-  return <video ref={video}></video>;
+  return (
+    <div>
+      <ul>
+        {comentarios.map((comentario) => (
+          <li key={comentario}>{comentario}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default useRef;
