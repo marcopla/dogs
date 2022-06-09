2a92240f81d2c90099bd85734529c8861caf19ee
Marco Pla <marco.pla@gmail.com>
2022-06-08 23:57:27 -0300

    useMemo

---
+++ react-app/src/useMemo.js
@@ -0,0 +1,12 @@
+https://www.origamid.com/slide/reacconst App = () => {
+  const [contar, setContar] = React.useState(0);
+  const valor = React.useMemo(() => {
+    const localStorageItem = window.localStorage.getItem('produto');
+    // só será executado uma vez
+    console.log('teste');
+    return localStorageItem;
+  }, []);
+  console.log(valor);
+
+  return <button onClick={() => setContar(contar + 1)}>{valor}</button>;
+};
