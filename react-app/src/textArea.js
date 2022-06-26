40a588a5c05adbc564f613307da9d8bfca971f13
Marco Pla <marco.pla@gmail.com>
2022-06-25 23:48:22 -0300

    textArea added

---
+++ react-app/src/textArea.js
@@ -0,0 +1,15 @@
+const App = () => {
+  const [mensagem, setMensagem] = React.useState('');
+
+  return (
+    <form>
+      <textarea
+        id="mensagem"
+        value={mensagem}
+        rows="5"
+        onChange={({ target }) => setMensagem(target.value)}
+      />
+      <p>{mensagem}</p>
+    </form>
+  );
+};
