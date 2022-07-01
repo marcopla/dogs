61661bd416ae74aaa8d5a5499fd530d36ca4dd9d
Marco Pla <marco.pla@gmail.com>
2022-06-30 23:53:55 -0300

    css Import added

---
+++ react-app/src/cssImport.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Title from './Components/Title';
+
+const App = () => {
+  return (
+    <div className="container">
+      {/* <Title text="Meu título" /> */}
+      <p className="text">Meu site</p>
+    </div>
+  );
+};
