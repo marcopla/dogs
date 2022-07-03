a53c102f7495befa64bba791cc2179dff2187ffc
Marco Pla <marco.pla@gmail.com>
2022-07-02 23:40:05 -0300

    styledComponents.js added

---
+++ react-app/src/styledComponents.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  color: tomato;
+`;
+
+const App = () => {
+  return (
+    <div>
+      <Title>Título principal</Title>
+    </div>
+  );
+};
