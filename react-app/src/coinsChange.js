140770d2bcf8f53038c2b3dbf0fc64d774c40cc1
Marco Pla <marco.pla@gmail.com>
2022-07-13 23:40:38 -0300

    function verifyIfCurrentBiggerThanCents added

--- react-app/src/coinsChange.js
+++ react-app/src/coinsChange.js
@@ -1,17 +1,34 @@
-const possibleCoinValues = [25, 10, 5, 1];
+const centsValues = [25, 10, 5, 1];
 
 const value = 15;
-let coinCounter = [0, 0, 0, 0];
-let count = value;
-function discountValue(value) {
-  possibleCoinValues.forEach((coinValue) => {
-    if (value >= coinValue) {
-      count = value - coinValue;
-      coinCounter[possibleCoinValues.indexOf(coinValue)]++;
-      console.log(coinCounter);
+const zeroArray = [0, 0, 0, 0];
+let coinCounter = zeroArray;
+let currentAmount = value;
+let previousAmount = currentAmount;
+let indexCoinValue = 0;
+let allCoinsCombinations = new Set();
+let indexSet = 0;
+
+function verifyIfCurrentBiggerThanCents(i, currentAmount){
+  if (currentAmount >= centsValues[i]) {
+    previousAmount = currentAmount;
+    currentAmount = currentAmount - centsValues[i];
+    coinCounter[i]++;
+    verifyIfCurrentBiggerThanCents(i, currentAmount);
+
+    if ((currentAmount !== 0) && () ){
+      discountValue(i, currentAmount);
+    } else {
+      allCoinsCombinations[indexSet++] = coinCounter;
+      coinCounter = zeroArray;
     }
-    discountValue(count);
-  });
+}
 }
 
-discountValue(value);
+function discountValue(indexCoinValue, currentAmount) {
+  for (let i = indexCoinValue; i < centsValues.length - 1; i++) {
+    verifyIfCurrentBiggerThanCents(i, currentAmount);
+  }
+}
+
+discountValue(indexCoinValue, currentAmount);
