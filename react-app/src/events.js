6e3c78dc2a8e95c7704514784ee9f6d3702d4b37
Marco Pla <marco.pla@gmail.com>
2022-07-25 23:57:54 -0300

    events.js added

---
+++ react-app/src/events.js
@@ -0,0 +1,12 @@
+const App = () => {
+  function handleClick(event) {
+    alert('Comprou: ' + event.target.innerText);
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick}>Camisa</button>
+      <button onClick={handleClick}>Bermuda</button>
+    </div>
+  );
+};
