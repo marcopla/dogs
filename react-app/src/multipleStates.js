081e21a02a2a3b05f2b445ae77ba01c2f2ae8320
Marco Pla <marco.pla@gmail.com>
2022-08-01 23:50:42 -0300

    multiple states added

---
+++ react-app/src/multipleStates.js
@@ -0,0 +1,7 @@
+const App = () => {
+  const [contar, setContar] = React.useState(0);
+  const [ativo, setAtivo] = React.useState(false);
+  const [dados, setDados] = React.useState({ nome: '', idade: '' });
+
+  return <div></div>;
+};
